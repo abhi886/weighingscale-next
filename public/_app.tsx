47860106674b72569d1c9ff93b8443e8bc40bc36
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-07-10 12:49:29 +1000

    Initial Commit - Next with typescript support

---
+++ public/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from "next/app";
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className='app-component'>
+      <Component {...pageProps} />
+    </div>
+  );
+}
+
+export default MyApp;
