1bc82e53717ffa77385045f8baaf01f2f234e067
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-07-12 10:34:35 +1000

    Configure PostCSS with TailwindCSS as a plugin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Layout } from "@components/common";
 export default function Home() {
-  return <p>Hello World</p>;
+  return (
+    <h1 className='text-3xl font-bold underline text-red-500'>Hello world</h1>
+  );
 }
 Home.layout = Layout;
