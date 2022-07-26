f172c8692f1757e90a092f5484efbee718a919bf
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-07-26 10:51:32 +1000

    Additional Design Fix: Fix Responsive for MonitorMostUsed Section to consume 2 cols grid in small screen

--- components/monitor/MonitorMostUsed/MonitorMostUsed.tsx
+++ components/monitor/MonitorMostUsed/MonitorMostUsed.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const MonitorMostUsed = () => {
   return (
-    <div className='grid grid-cols-3 gap-2 '>
-      <div className='grid col-span-2'>
+    <div className='grid grid-cols-2 md:grid-cols-3 gap-2 '>
+      <div className='grid md:col-span-2'>
         <p>Most Used Products</p>
       </div>
 
-      <div className='grid grid-cols-2 text-xs h-28 gap-1 place-items-center text-center pt-2'>
+      <div className='grid grid-cols-2 text-xs md:h-28 gap-1 place-items-center text-center pt-2'>
         <div className='bg-green-900 rounded-lg text-white w-full h-8 px-2'>
           Small Tub
         </div>
